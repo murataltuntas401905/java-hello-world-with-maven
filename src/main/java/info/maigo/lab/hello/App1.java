5aec6b52fcf58642a4c77899be15974abf3f0542
murataltuntas401905 <murat.altuntas401905@gmail.com>
2021-03-13 12:40:31 +0000

    java maven added simple app1 added1

--- src/main/java/info/maigo/lab/hello/App1.java
+++
@@ -1,9 +0,0 @@
-package info.maigo.lab.hello;
-
-public class App1 {
-
-  public static void main(String[] args) {
-    System.out.println( "Hello World!" );
-  }
-}
-
--- src/test/java/info/maigo/lab/hello/AppTest.java
+++ src/test/java/info/maigo/lab/hello/AppTest.java
@@ -4,23 +4,36 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class AppTest extends TestCase {
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
 
-  public AppTest(String testName) {
-    super(testName);
-  }
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
 
-  public static Test suite() {
-    return new TestSuite( AppTest.class );
-  }
-
-  /**
-   * getUserInfoURL Test
-   */
-  public void testGetUserInfoURL() {
-    String name = "niwasawa";
-    App app = new App(name);
-    assertEquals("https://api.github.com/users/niwasawa", app.getUserInfoURL());
-  }
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
 }
 
